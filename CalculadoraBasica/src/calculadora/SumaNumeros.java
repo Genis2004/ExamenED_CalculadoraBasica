da3ffe48286191a98f98ca7d3b57a77b3c4c43a0
genis <grodriguez19012@iesjoanramis.org>
2024-12-12 18:45:28 +0100

    Comentario después de realizar fusión entre ramas

--- CalculadoraBasica/src/calculadora/SumaNumeros.java
+++ CalculadoraBasica/src/calculadora/SumaNumeros.java
@@ -20,4 +20,6 @@ public class SumaNumeros {
 		
 		System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
 	}
+	//comentaro des de la rama
+	//Comentario después de realizar fusión entre ramas
 }
